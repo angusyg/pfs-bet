d6d23f4f58ffb06d90a016b3f39f93c25467b1f4
gaelclair <gael.clair@gmail.com>
2018-06-21 17:13:10 +0200

    Navbar click handler

--- frontend/app/components/navbar/navbar.controller.js
+++ frontend/app/components/navbar/navbar.controller.js
@@ -12,5 +12,19 @@
 
   function NavbarController() {
     const vm = this;
+
+    /** variables */
+    vm.HOME_TAB = 'home';
+    vm.PRONOS_TAB = 'pronos';
+    vm.RANKING_TAB = 'ranking';
+    vm.BONUS_TAB = 'bonus';
+    vm.selectedTab = vm.HOME_TAB;
+
+    /** functions */
+    vm.selectTab = selectTab;
+
+    function selectTab(tab) {
+      vm.selectedTab = tab;
+    }
   }
 }());
